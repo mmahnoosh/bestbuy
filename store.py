9ae35ffe7258d3a3be3e0c3876c9c60c4af143f4
mmahnoosh <montazer_mahnoosh@gmx.de>
2025-05-06 20:56:13 +0200

    Implement Product class with validation

--- store.py
+++ store.py
@@ -1,1 +1,13 @@
-https://codio.com/mmontazer/best-buy/tree/store.py#
+class Product:
+
+    def __init__(self, name, price, quantity):
+        if not name:
+            raise ValueError(
+                "Invalid input: name must not be empty")
+        if price < 0 or quantity < 0:
+            raise ValueError(
+                "Invalid input: price and quantity must be non-negative.")
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
