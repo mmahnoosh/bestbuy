766b82a4b302479c5aef4782ddaf3e98dda4de1b
mmahnoosh <montazer_mahnoosh@gmx.de>
2025-05-07 10:42:34 +0200

    Implement Store class with product management and add_product method

--- store.py
+++ store.py
@@ -1,52 +1,13 @@
-class Product:
+class Store:
 
-    def __init__(self, name, price, quantity):
-        if not name:
-            raise ValueError(
-                "Invalid input: name must not be empty")
-        if price < 0 or quantity < 0:
-            raise ValueError(
-                "Invalid input: price and quantity must be non-negative.")
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-        self.active = True
+    def __init__(self, products=None):
+        if products is None:
+            products = []
+        self.products = products
 
+    def add_product(self, product):
+        if not hasattr(product, 'name') or not hasattr(product, 'price') or not hasattr(product,
+                                                                                        'quantity'):
+            raise TypeError("Product must have 'name', 'price', and 'quantity' attributes")
+        self.products.append(product)
 
-    def get_quantity(self) -> int:
-        return self.quantity
-
-
-    def set_quantity(self, quantity):
-        self.quantity = quantity
-        if quantity == 0:
-            self.active = False
-
-    def is_active(self) -> bool:
-        return self.active
-
-
-    def activate(self):
-        self.active = True
-
-
-    def deactivate(self):
-        self.active = False
-
-
-    def show(self) -> str:
-        status = "Active" if self.active else "Inactive"
-        return f'"{self.name}, Price: {self.price}, Quantity: {self.quantity}, Status: {status}"'
-
-    def buy(self, quantity) -> float:
-        if quantity <= 0:
-            raise ValueError("Invalid input: quantity must be greater than 0.")
-        if quantity > self.quantity:
-            raise ValueError("Not enough stock: there is not enough product available.")
-
-        total_price = quantity * self.price
-        self.quantity -= quantity  # Shorter way of updating the quantity
-        return total_price
-
-product = Product("MacBook Air M2",1450, 100)
-print(product.show())
